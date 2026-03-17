ef0627cb39120be7728867b7bd7b32880a7bdd00
Oleg <oleg@digiway.dev>
2026-03-17 10:34:23 +0200

    Update template.php

--- template-parts/gutenberg-blocks/all-posts/template.php
+++ template-parts/gutenberg-blocks/all-posts/template.php
@@ -19,7 +19,7 @@ if (!empty($block['className'])) $block_classes .= ' ' . $block['className'];
                 <?php endif; ?>
 
                 <button class="all-posts__filter">
-                    ФИЛЬТР
+                    ФІЛЬТР
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g clip-path="url(#clip0_3836_2854)">
                             <path d="M21.0039 3.99999H14.0039" stroke="currentColor" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round"/>
