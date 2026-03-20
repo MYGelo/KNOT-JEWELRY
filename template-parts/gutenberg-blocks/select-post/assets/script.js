0d2c6a759d9e383552e068ad3ef784fe216c84da
Oleg <oleg@digiway.dev>
2026-03-21 00:00:27 +0200

    fix

--- template-parts/gutenberg-blocks/select-post/assets/script.js
+++ template-parts/gutenberg-blocks/select-post/assets/script.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
     new Swiper('.select-post__swiper', {
         speed: 600,
-        // parallax: true,
+        parallax: true,
         slidesPerView: 3,
-        spaceBetween: 30,
-        centeredSlides: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
+        spaceBetween: 24,
+        // pagination: {
+        //     el: '.select_post .swiper-pagination',
+        //     clickable: true,
+        // },
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.select_post__swiper-button-next',
+            prevEl: '.select_post__swiper-button-prev',
         },
-        loop: true,
+        // loop: true,
+        // centeredSlides: true,
 
         breakpoints: {
             0: {
-                slidesPerView: 1
+                slidesPerView: 1,
+                // spaceBetween: 50,
             },
             551: {
-                slidesPerView: 2
+                slidesPerView: 2,
+                spaceBetween: 12,
             },
             767: {
-                slidesPerView: 3
+                slidesPerView: 3,
+                spaceBetween: 18,
             },
         }
     });
