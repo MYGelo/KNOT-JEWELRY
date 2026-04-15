d872bbc750db8c7fdd30879f50c57ac3c840bae3
oleg-digiway <oleg@digiway.dev>
2026-04-15 18:18:54 +0300

    Revert "1"

--- inc/telegram-bot.php
+++ inc/telegram-bot.php
@@ -38,17 +38,7 @@ function knot_send_telegram_message() {
 
 	$status_code = wp_remote_retrieve_response_code($response);
 	if ($status_code < 200 || $status_code >= 300) {
-		$body = wp_remote_retrieve_body($response);
-		$error_message = 'Telegram API error';
-
-		if (!empty($body)) {
-			$decoded = json_decode($body, true);
-			if (!empty($decoded['description'])) {
-				$error_message = $decoded['description'];
-			}
-		}
-
-		wp_send_json_error(['message' => $error_message], 500);
+		wp_send_json_error(['message' => 'Telegram API error'], 500);
 	}
 
 	wp_send_json_success(['sent' => true]);
