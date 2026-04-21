b23d985b879e7c8e56e6366a5a4eb95d5962308e
Oleg <oleg@digiway.dev>
2026-04-21 22:38:45 +0300

    in stock speed

--- functions.php
+++ functions.php
@@ -17,3 +17,5 @@ require_once __DIR__ . '/inc/ajax-filter.php';
 require_once __DIR__ . '/inc/helpers/cf7-info-single.php';
 
 require_once __DIR__ . '/inc/helpers/comments.php';
+
+require_once __DIR__ . '/inc/cash.php';
