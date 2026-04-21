ff2496835043c9e6bdb36dcd9fd4b65096790454
Oleg <112314820+MYGelo@users.noreply.github.com>
2026-04-21 23:38:24 +0300

    Revert "Speed"

--- functions.php
+++ functions.php
@@ -17,5 +17,3 @@ require_once __DIR__ . '/inc/ajax-filter.php';
 require_once __DIR__ . '/inc/helpers/cf7-info-single.php';
 
 require_once __DIR__ . '/inc/helpers/comments.php';
-
-require_once __DIR__ . '/inc/cash.php';
