f730d0e854ff35223aa47cca4146c0450b6b0cab
Oleg <oleg@digiway.dev>
2026-04-24 11:57:27 +0300

    swiper fix

--- template-parts/gutenberg-blocks/reviews/assets/script.js
+++ template-parts/gutenberg-blocks/reviews/assets/script.js
@@ -5,12 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
         new Swiper(slider, {
             slidesPerView: 'auto',
             spaceBetween: 16,
-            autoplay: {
-                delay: 4000,
-            },
-            freeMode: false,
-            speed: 500,
+
             loop: true,
+            centeredSlides: true,
+
+            grabCursor: true,
+            allowTouchMove: true,
+
+            freeMode: {
+                enabled: true,
+                momentum: true,
+                momentumRatio: 1,
+                momentumVelocityRatio: 1,
+            },
+
+            autoplay: {
+                delay: 0,
+                disableOnInteraction: false,
+            },
+
+            speed: 3000,
         });
     }
 });
