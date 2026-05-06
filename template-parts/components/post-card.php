e6f9b3b147eced9ce3d1034263f8d22960b1ad86
Oleg <oleg@digiway.dev>
2026-05-06 20:11:51 +0300

    fix all posts cash

--- template-parts/components/post-card.php
+++ template-parts/components/post-card.php
@@ -1,30 +1,29 @@
 <?php
+$post_id = get_the_ID();
 $price_meta = get_post_meta(get_the_ID(), 'price', true);
 ?>
 
 <div class="all-posts__post-item">
 
-    <a href="<?= esc_url(get_permalink()) ?>"
+    <a href="<?= esc_url(get_permalink($post_id)) ?>"
        class="all-posts__post-thumb image-wrapper">
 
-        <?= get_the_post_thumbnail(
-            get_the_ID(),
-            'medium',
-            ['loading' => 'lazy']
-        ); ?>
+        <?php if (has_post_thumbnail($post_id)) : ?>
+
+            <?= get_the_post_thumbnail(
+                $post_id,
+                'medium',
+                ['loading' => 'lazy']
+            ); ?>
+
+        <?php endif; ?>
 
     </a>
 
     <div class="all-post__text-content">
-
         <h2 class="all-posts__item-title">
             <?php the_title(); ?>
         </h2>
-
-
-
-
-
     </div>
 
 
